403401bdd5b191956624078058a281aea119f631
31352286 <31352286@099141-P33S102.macklabs.br>
2014-09-26 09:17:08 -0300

    TesteMulher pronto !!

--- IMC/test/PessoaTest.java
+++ IMC/test/PessoaTest.java
@@ -27,14 +27,27 @@ public class PessoaTest {
     
     @Test
     public void testExibeResultado(){
-        
-        String mulher01 = "IMC: 17,6 Procure um médico, você vai morrer";
-        
-        double peso = 45;
-        double altura = 160;
-        String mulher02 = "IMC: 17,6\nVocê está abaixo do peso recomendado!";
         Pessoa pessoa = new Pessoa();
-        Assert.assertEquals(mulher02, pessoa.exibeResultado("Mulher", peso, altura));
+        double peso = 30;
+        double altura = 160;
+        String mulher01 = "IMC: 11,7\nProcure um médico, você vai morrer";
+        Assert.assertEquals(mulher01, pessoa.exibeResultado("Mulher", peso, altura));
+        
+        double peso1 = 45;
+        String mulher02 = "IMC: 17,6\nVocê está abaixo do peso recomendado!";
+        Assert.assertEquals(mulher02, pessoa.exibeResultado("Mulher", peso1, altura));
+        
+        double peso2 = 50;
+        String mulher03 = "IMC: 19,5\nVocê está muito bem! Continue assim!";
+        Assert.assertEquals(mulher03, pessoa.exibeResultado("Mulher", peso2, altura));
+        
+        double peso3 = 65;
+        String mulher04 = "IMC: 25,4\nVocê está acima do peso recomendado. Cuidado!";
+        Assert.assertEquals(mulher04, pessoa.exibeResultado("Mulher", peso3, altura));
+        
+        double peso4 = 75;
+        String mulher05 = "IMC: 29,3\nVocê está Obeso. Procure o acompanhamento de um nutricionista e realizar mais atividades físicas!";
+        Assert.assertEquals(mulher05, pessoa.exibeResultado("Mulher", peso4, altura));
     
     }
 }
