617555f3d4d885bb2e527f4de289c3e2751416e8
Vivi <Vivi@ViviBook>
2014-10-19 18:40:14 -0200

    Coloquei else's no metodo de mostra o resultado

--- IMC/src/Pessoa.java
+++ IMC/src/Pessoa.java
@@ -52,45 +52,48 @@ public class Pessoa {
                 fd = df.format(imc);
                 saida = "IMC: "+fd+"\nProcure um médico, você vai morrer";
             }
-            if(imc>=15 && imc<18){
+            else if(imc>=15 && imc<18){
                 fd = df.format(imc);
                 saida = "IMC: "+fd+"\nVocê está abaixo do peso recomendado!";
             }
-            if(imc>=18 && imc<24.5){
+            else if(imc>=18 && imc<24.5){
                 fd = df.format(imc);
                 saida = "IMC: "+fd+"\nVocê está muito bem! Continue assim!";            
             }
-            if(imc>=24.5 && imc<27.3){
+            else if(imc>=24.5 && imc<27.3){
                 fd = df.format(imc);
                 saida = "IMC: "+fd+"\nVocê está acima do peso recomendado. Cuidado!";
             }
-            if(imc>=27.3){
+            else{
                 fd = df.format(imc);
                 saida = "IMC: "+fd+"\nVocê está Obeso. Procure o acompanhamento de um nutricionista e realizar mais atividades físicas!";
             }
         }
         
-        if(sexo.equalsIgnoreCase("Homem")){
+        else if(sexo.equalsIgnoreCase("Homem")){
             if(imc<17.9){
                 fd = df.format(imc);
                 saida = "IMC: "+fd+"\nProcure um médico, você vai morrer";
             }
-            if(imc>=17.9 && imc<19){
+            else if(imc>=17.9 && imc<19){
                 fd = df.format(imc);
                 saida = "IMC: "+fd+"\nVocê está abaixo do peso recomendado!";
             }
-            if(imc>=19 && imc<25){
+            else if(imc>=19 && imc<25){
                 fd = df.format(imc);
                 saida = "IMC: "+fd+"\nVocê está muito bem! Continue assim!";            
             }
-            if(imc>=25 && imc<27.8){
+            else if(imc>=25 && imc<27.8){
                 fd = df.format(imc);
                 saida = "IMC: "+fd+"\nVocê está acima do peso recomendado. Cuidado!";
             }
-            if(imc>=27.8){
+            else{
                 fd = df.format(imc);
                 saida = "IMC: "+fd+"\nVocê está Obeso. Procure o acompanhamento de um nutricionista e realizar mais atividades físicas!";
             }
+        }
+        else{
+            saida = "Sexo não existe";
         }
        return saida;
     }
